0937f5f4e42880ebeee736a89a23703a484c9fe2
John Page <jdehpage@gmail.com>
2019-01-29 09:36:54 +0000

    game code formatting error fix

--- rock_paper_scissors/models/rps_game.rb
+++ rock_paper_scissors/models/rps_game.rb
@@ -8,7 +8,7 @@ class RPSgame
   def sign_comparison()
     if @player1 == @player2
       return "Draw"
-    elsif (@player1 == "Rock" && @player2 == "Scissors") || (@player1 == "Paper" && @player2 == "Rock") || (@player1 == "Scissors" && @player2 == "Paper")
+    elsif (@player1 == "rock" && @player2 == "scissors") || (@player1 == "paper" && @player2 == "rock") || (@player1 == "scissors" && @player2 == "paper")
       return "Player1 wins with #{@player1}"
     else
       return "Player2 wins with #{@player2}"
